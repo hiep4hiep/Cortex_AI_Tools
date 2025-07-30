8ffd964fe46908e0a85342d3e595be50ee0fe64a
Hiep <hiep4hiep@gmail.com>
2025-07-30 15:19:06 +1000

    update run app

--- run_app.sh
+++ run_app.sh
@@ -1,3 +1,5 @@
+docker build -t flask-gunicorn .
 docker run -d --rm -p 8000:8000 -v "$PWD/XSIAM_SPL_Converter":/app flask-gunicorn
 docker run -d --rm -p 8001:8000 -v "$PWD/XSIAM_DM_Generator":/app flask-gunicorn
 docker run -d --rm -p 8002:8000 -v "$PWD/XSIAM_Ingestion_Document":/app flask-gunicorn
+docker container ps
