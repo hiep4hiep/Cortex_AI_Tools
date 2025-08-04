9f7f45fa417db0f364c31509d66d6db514c8a7f5
Hiep <hiep4hiep@gmail.com>
2025-08-04 15:03:42 +1000

    Update certbot

--- Web_App/app.py
+++ Web_App/app.py
@@ -14,6 +14,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', response="", loading=False)
 
+@app.route("/headers", methods=["GET", "POST", "PUT", "DELETE", "PATCH"])
+def show_headers():
+    headers = dict(request.headers)
+    return jsonify(headers), 200
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
