271371b06ff8868d648a57187d90a96900df210a
Hiep <hiep4hiep@gmail.com>
2025-08-04 17:42:43 +1000

    add sub pages

--- Web_App/app.py
+++ Web_App/app.py
@@ -14,10 +14,24 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', response="", loading=False)
 
+@app.route('/spl', methods=['GET'])
+def spl():
+    return render_template('spl.html')
+
+@app.route('/dmgen', methods=['GET'])
+def dmgen():
+    return render_template('dmgen.html')
+
+@app.route('/ingestion', methods=['GET'])
+def ingestion():
+    return render_template('ingestion.html')
+
 @app.route("/headers", methods=["GET", "POST", "PUT", "DELETE", "PATCH"])
 def show_headers():
     headers = dict(request.headers)
     return jsonify(headers), 200
+
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
